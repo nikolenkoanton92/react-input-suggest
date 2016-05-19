daf3a0508ab3a1308185d3438d06c7b376803f41
Anton Nikolenko <nikolenkoanton92@gmail.com>
2016-05-18 22:01:10 -0700

    include style to react-input-suggest component

--- example/app/app.js
+++ example/app/app.js
@@ -1,11 +1,16 @@
+import 'react-input-suggest.css'
+import './style.css'
+
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import ReactInputSuggest from 'react-input-suggest'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
       <h1>Example App</h1>
+      <ReactInputSuggest />
       </div>
       )
   }
