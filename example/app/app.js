a39b35efc2c8856c5f89434c1283b44f74b2254d
Anton Nikolenko <nikolenkoanton92@gmail.com>
2016-05-29 23:27:41 -0700

    add to example STATES

--- example/app/app.js
+++ example/app/app.js
@@ -5,12 +5,14 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import ReactInputSuggest from 'react-input-suggest'
 
+import STATES from './data/states'
+
 class App extends Component {
   render() {
     return (
       <div className="container">
       <h1>Example App</h1>
-      <ReactInputSuggest />
+      <ReactInputSuggest suggestions={STATES}/>
       </div>
       )
   }
